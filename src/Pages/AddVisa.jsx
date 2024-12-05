644d80fd09bca950981848761fb866d8a2abfa22
AsifurRahman10 <1720105@iub.edu.bd>
2024-12-05 16:17:04 +0600

    add visa page added

--- src/Pages/AddVisa.jsx
+++ src/Pages/AddVisa.jsx
@@ -1,118 +1,137 @@
-import passport from "../assets/passport.jpg";
-import { Link } from "react-router";
-import { Title } from "../Components/Title";
-
 export const AddVisa = () => {
   return (
-    <div className="min-h-[calc(100vh-404px)] my-20">
-      <Title title={"Add Visa"}></Title>
-      <div className="flex box-border justify-center items-center py-6">
-        <div className="rounded-2xl flex max-w-5xl p-5 items-center">
+    <div className="py-10 lg:py-14">
+      <div className="flex box-border justify-center items-center">
+        <div className="rounded-2xl flex w-11/12 md:max-w-5xl md:p-5 items-center">
           <div className="mx-auto px-8">
-            <h2 className="font-bold text-3xl text-primary">
+            <h2 className="font-bold text-xl md:text-3xl text-primary mb-6">
               Please fill the form with correct information
             </h2>
 
-            <form className="flex flex-col gap-4">
+            <form className="flex flex-col gap-4" onSubmit={handleFormSubmit}>
               <div>
-                <label>Country image</label>
+                <label className="font-medium">Country name</label>
+                <input
+                  type="text"
+                  name="cName"
+                  className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                  placeholder="Country Name"
+                />
+              </div>
+              <div>
+                <label className="font-medium">Country image</label>
                 <input
                   type="text"
                   name="cPhoto"
                   className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                  value=""
-                  placeholder="Country image"
-                />
-              </div>
-              <div>
-                <label>Country name</label>
-                <input
-                  type="text"
-                  name="cName"
-                  className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                  value=""
-                  placeholder="Country image"
-                />
-              </div>
-              <div>
-                <label>Country name</label>
-                <input
-                  type="text"
-                  name="cName"
-                  className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                  value=""
                   placeholder="Country image"
                 />
               </div>
 
-              {/* options */}
-              <select className="select select-bordered w-full max-w-xs">
-                <option disabled selected>
-                  Visa_type
-                </option>
-                <option>Tourist visa</option>
-                <option>Student visa</option>
-                <option>Official visa</option>
-              </select>
+              <div className="flex flex-col md:flex-row justify-between items-center gap-2">
+                <div className="flex-1">
+                  <label className="font-medium">Processing time (day) </label>
+                  <input
+                    type="number"
+                    name="time"
+                    className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                    placeholder="Processing time"
+                  />
+                </div>
+                {/* options */}
+                <div className="flex-1 w-full">
+                  <label className="font-medium"> Visa Type</label>
+                  <select className="select select-bordered mt-1 h-10 w-full bg-gray-50">
+                    <option selected disabled>
+                      Visa Type
+                    </option>
+                    <option>Tourist visa</option>
+                    <option>Student visa</option>
+                    <option>Official visa</option>
+                  </select>
+                </div>
+              </div>
+              <label className="font-medium">Required_documents</label>
+              <div className="grid grid-cols-2 md:grid-cols-3 justify-center gap-2 mt-1">
+                <div className="flex items-start gap-2">
+                  <input type="checkbox" checked="" class="checkbox" />
+                  <span>Valid passport</span>
+                </div>
+                <div className="flex items-start gap-2">
+                  <input type="checkbox" checked="" class="checkbox" />
+                  <span>Visa application form</span>
+                </div>
+                <div className="flex items-start gap-2">
+                  <input type="checkbox" checked="" class="checkbox" />
+                  <span>Recent passport-sized photograph</span>
+                </div>
+                <div className="flex items-start gap-2">
+                  <input type="checkbox" checked="" class="checkbox" />
+                  <span>Proof of financial means</span>
+                </div>
+                <div className="flex items-start gap-2">
+                  <input type="checkbox" checked="" class="checkbox" />
+                  <span>Proof of accommodation</span>
+                </div>
+                <div className="flex items-start gap-2">
+                  <input type="checkbox" checked="" class="checkbox" />
+                  <span>Travel itinerary</span>
+                </div>
+              </div>
+
+              <div className="w-full">
+                <label className="font-medium">Description</label>
+                <br />
+                <textarea
+                  class="textarea textarea-bordered w-full mt-1 bg-gray-50"
+                  placeholder="Enter visa Description"
+                ></textarea>
+              </div>
+
+              <div className="flex flex-col md:flex-row justify-center items-center gap-2">
+                <div className="flex-1">
+                  <label className="font-medium">Age_restriction</label>
+                  <input
+                    type="number"
+                    name="cName"
+                    className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                    placeholder="Enter minimum age for apply"
+                  />
+                </div>
+                <div className="flex-1 w-full">
+                  <label className="font-medium">Fee $</label>
+                  <input
+                    type="number"
+                    name="cName"
+                    className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                    placeholder="Enter Fee"
+                  />
+                </div>
+              </div>
+              <div className="flex-1">
+                <label className="font-medium">Validity</label>
+                <input
+                  type="text"
+                  name="cName"
+                  className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                  placeholder="Enter visa validity"
+                />
+              </div>
+              <div className="flex-1">
+                <label className="font-medium">Application_method</label>
+                <input
+                  type="text"
+                  name="cName"
+                  className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                  placeholder="Ex - online, offline, embassy"
+                />
+              </div>
+
               <button className="relative btn bg-primary text-white font-bold text-lg lato rounded-xl w-full overflow-hidden group">
                 <span className="absolute inset-0 bg-secondary transform -translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-in-out"></span>
                 <span className="relative z-10">Login</span>
               </button>
             </form>
-            <div className="mt-6  items-center text-gray-100">
-              <hr className="border-gray-300" />
-              <p className="text-center text-sm">OR</p>
-              <hr className="border-gray-300" />
-            </div>
-            <button
-              //   onClick={handleGoogleLogin}
-              className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 hover:bg-[#60a8bc4f] font-medium"
-            >
-              <svg
-                className="mr-3"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 48 48"
-                width="25px"
-              >
-                <path
-                  fill="#FFC107"
-                  d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-                ></path>
-                <path
-                  fill="#FF3D00"
-                  d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-                ></path>
-                <path
-                  fill="#4CAF50"
-                  d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-                ></path>
-                <path
-                  fill="#1976D2"
-                  d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-                ></path>
-              </svg>
-              Login with Google
-            </button>
-            <div className="mt-10 text-sm border-b border-gray-500 py-5 playfair tooltip">
-              Forget password?
-            </div>
-
-            <div className="mt-4 text-sm flex justify-between items-center container-mr">
-              <p className="mr-3 md:mr-0 ">If you don't have an account..</p>
-              <Link to={"/register"}>
-                <button className="relative btn bg-primary text-white font-bold text-lg lato rounded-xl     overflow-hidden group">
-                  <span className="absolute inset-0 bg-secondary transform -translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-in-out"></span>
-                  <span className="relative z-10">Register</span>
-                </button>
-              </Link>
-            </div>
-          </div>
-          <div className="md:block hidden w-1/2">
-            <img
-              className="rounded-2xl max-h-[1600px]"
-              src={passport}
-              alt="login form image"
-            />
           </div>
         </div>
       </div>
